5e2c562540c2522c934eb283681b530963fa3042
andy3471 <andy@insideindie.net>
2018-02-19 00:48:41 +0100

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// DB Connection
+define('DB_SERVER', '192.168.1.52');
+define('DB_USERNAME', 'key');
+define('DB_PASSWORD', 'cuntflaps69');
+define('DB_NAME', 'keyshare');
+
+// Config
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+define("SECURE", FALSE);
+define('TITLE',"360NoHope Keyshare");
+?>
