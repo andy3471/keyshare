98aa3a5f976b3c72dcfc258589365a9e0df908b1
andy3471 <andy@insideindie.net>
2018-02-19 09:34:30 +0000

    IgnoreConfig

--- includes/config.php
+++ includes/config.php
@@ -7,8 +7,6 @@ define('DB_PASSWORD', 'cuntflaps69');
 define('DB_NAME', 'keyshare');
 
 // Config
-define("CAN_REGISTER", "any");
-define("DEFAULT_ROLE", "member");
-define("SECURE", FALSE);
 define('TITLE',"Test Keyshare");
+
 ?>
