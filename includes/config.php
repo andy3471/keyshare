7e98315e03da73babe7696ed6ec097c9ec82f61c
andy3471 <andy@insideindie.net>
2018-02-19 09:33:30 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
 
 // DB Connection
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'keyshare');
-define('DB_PASSWORD', 'cuntflaps69');
-define('DB_NAME', 'keyshare');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
 
 // Config
 define("CAN_REGISTER", "any");
