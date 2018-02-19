1ba874c534750f89053bdae96afd0f2e60926f6c
andy3471 <andy@insideindie.net>
2018-02-19 15:57:50 +0000

    PageNumbers

--- games.php
+++ games.php
@@ -61,14 +61,26 @@ $row = $result->fetch_assoc();
 $total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
   
 			echo '<ul class="pagination justify-content-center">';
+			
+			if ($page == 1) {
+				echo "<li class='disabled'><a href='#'>Previous</a></li>";
+			} else {
+				echo "<li><a href='?page=".($page-1)."'>Previous</a></li>";
+			}
+			
 for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
             echo "<li";
-			if ($i==$page)  echo " class='active'";
-			echo "><a href='games.php?page=".$i."'";
-            echo ">".$i."</a></li> "; 
-}; 
-			echo '<ul>';
-?>
-                        
+				if ($i==$page)  echo " class='active'";
+			echo "><a href='?page=".$i."'";
+            echo ">".$i."</a></li>";
+		}
 
-<?php include './theme/footer.php';?>
+			if ($page == $total_pages) {
+				echo "<li class='disabled'><a href='#'>Next</a></li>";
+			} else {
+				echo "<li><a href='?page=".($page+1)."'>Next</a></li>";
+			}
+			
+			echo "<ul>";
+			
+ include './theme/footer.php';?>
