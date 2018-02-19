9d2d01a783748cdb88bca476ce85891fca6cd7d8
andy3471 <andy@insideindie.net>
2018-02-19 15:34:26 +0000

    SplitGamesIntoPages

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,14 @@
 <?php
 
 // DB Connection
-define('DB_SERVER', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_NAME', '');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'keyshare');
+define('DB_PASSWORD', 'cuntflaps69');
+define('DB_NAME', 'keyshare');
 
 // Config
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+define("SECURE", FALSE);
 define('TITLE',"Test Keyshare");
-
 ?>
