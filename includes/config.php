a3d807e54fc911afe9be526e9449c43a4d28302e
andy3471 <andy@insideindie.net>
2018-02-19 09:35:21 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -7,8 +7,5 @@ define('DB_PASSWORD', '');
 define('DB_NAME', '');
 
 // Config
-define("CAN_REGISTER", "any");
-define("DEFAULT_ROLE", "member");
-define("SECURE", FALSE);
 define('TITLE',"Test Keyshare");
 ?>
