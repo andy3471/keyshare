555b84ab2ca32dcd8327c7898d484024d69d690f
andy3471 <andy@insideindie.net>
2018-02-20 17:20:59 +0000

    FixMultiplePagesLosingGetValue

--- viewuser.php
+++ viewuser.php
@@ -1,5 +1,16 @@
 <?php include './theme/header.php';
 
+$results_per_page = 15;
+
+if (isset($_GET["page"])) { 
+	$page  = $_GET["page"]; 
+} 
+else { 
+	$page=1; 
+}; 
+
+$start_from = ($page-1) * $results_per_page;
+
 //get id from url, else redirect
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
@@ -51,6 +62,7 @@ if ($result->num_rows > 0) {
         where K.created_user_id = $id
         and removed is null
         and owned_user is null
+		LIMIT $start_from, $results_per_page
         ";
 
         $keyresults = $mysqli->query($fetchkeys);
@@ -66,15 +78,49 @@ if ($result->num_rows > 0) {
             echo "0 Donated Keys";
         }
             echo '</table>';
-    echo '</div>
-          </div>';
        }
+
+   $sql = "
+        select count(K.key_id) as total from keyshare.keys AS K
+        where K.created_user_id = $id
+        and removed is null
+        and owned_user is null";
+
+$result = $mysqli->query($sql);
+$row = $result->fetch_assoc();
+$total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
+  
+			echo '<ul class="pagination justify-content-center">';
+			
+			if ($page == 1) {
+				echo "<li class='disabled'><a href='#'>Previous</a></li>";
+			} else {
+				echo "<li><a href='?page=".($page-1)."'>Previous</a></li>";
+			}
+			
+for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
+            echo "<li";
+				if ($i==$page)  echo " class='active'";
+			echo "><a href='?page=".$i."'";
+            echo ">".$i."</a></li>";
+		}
+
+			if ($page == $total_pages) {
+				echo "<li class='disabled'><a href='#'>Next</a></li>";
+			} else {
+				echo "<li><a href='?id=$id&page=".($page+1)."'>Next</a></li>";
+			}
+			
+			echo "<ul>";
 
    
    
 } else {
    header('Location: topusers.php');
 }
+
+
+
 
 include './theme/sidebar.php';
 include './theme/footer.php';?>
