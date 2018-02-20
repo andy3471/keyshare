4bfca839311203116402b89bd3784f56e050d76f
BuildTools <unconfigured@null.spigotmc.org>
2018-02-20 22:46:17 +0000

    InstallScript2

--- install/init_db.php
+++ install/init_db.php
@@ -22,6 +22,7 @@ $forename = test_input($_POST['forename']);
 $surname = test_input($_POST['surname']);
 $email = test_input($_POST['email']);
 
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
 //SQL
     
@@ -31,7 +32,7 @@ $query= "CREATE TABLE `dlc` (
   `game_id` varchar(45) NOT NULL,
   `created_user_id` int(11) NOT NULL,
   `created_dttm` datetime NOT NULL
-); ";
+) ";
 
 $query .= "CREATE TABLE `games` (
   `game_id` int(11) NOT NULL,
@@ -39,7 +40,7 @@ $query .= "CREATE TABLE `games` (
   `description` varchar(255) DEFAULT NULL,
   `created_user_id` int(11) DEFAULT NULL,
   `created_dttm` varchar(45) DEFAULT NULL
-); ";
+) ";
 
 $query .= "CREATE TABLE `keys` (
   `key_id` int(11) NOT NULL,
@@ -51,13 +52,13 @@ $query .= "CREATE TABLE `keys` (
   `removed` binary(1) DEFAULT NULL,
   `created_user_id` int(11) DEFAULT NULL,
   `created_dttm` varchar(45) DEFAULT NULL
-); ";
+) ";
 
 $query .= "CREATE TABLE `platforms` (
   `platform_id` int(11) NOT NULL,
   `platformname` varchar(45) DEFAULT NULL,
   `created_dttm` varchar(45) DEFAULT NULL
-); ";
+) ";
 
 $query .= "INSERT INTO `platforms` (`platform_id`, `platformname`, `created_dttm`) VALUES
 (1, 'Steam', '2018-02-04'),
@@ -76,7 +77,7 @@ $query .= "INSERT INTO `platforms` (`platform_id`, `platformname`, `created_dttm
 $query .= "CREATE TABLE `roles` (
   `role_id` int(11) NOT NULL,
   `Name` varchar(45) NOT NULL
-); ";
+) ";
 
 $query .= "INSERT INTO `roles` (`role_id`, `Name`) VALUES
 (2, 'Admin'),
@@ -92,19 +93,19 @@ $query .= "CREATE TABLE `users` (
   `role_id` int(11) DEFAULT NULL,
   `create_date` datetime NOT NULL,
   `approved` int(1) DEFAULT NULL
-); ";
+) ";
 
 
 $query .= "ALTER TABLE `dlc`
   ADD PRIMARY KEY (`dlc_id`) USING BTREE,
   ADD UNIQUE KEY `dlcname` (`dlcname`),
   ADD KEY `game_id` (`game_id`),
-  ADD KEY `created_user_id` (`created_user_id`); ";
+  ADD KEY `created_user_id` (`created_user_id`);";
 
 $query .= "ALTER TABLE `games`
   ADD PRIMARY KEY (`game_id`),
   ADD UNIQUE KEY `gamename` (`gamename`),
-  ADD KEY `created_user_id` (`created_user_id`); ";
+  ADD KEY `created_user_id` (`created_user_id`);";
 
 $query .= "ALTER TABLE `keys`
   ADD PRIMARY KEY (`key_id`),
@@ -113,62 +114,73 @@ $query .= "ALTER TABLE `keys`
   ADD KEY `game_id` (`game_id`),
   ADD KEY `platform_id` (`platform_id`),
   ADD KEY `created_user_id` (`created_user_id`),
-  ADD KEY `owned_user` (`owned_user`); ";
+  ADD KEY `owned_user` (`owned_user`);";
 
 $query .= "ALTER TABLE `platforms`
   ADD PRIMARY KEY (`platform_id`),
-  ADD UNIQUE KEY `platformname` (`platformname`); ";
+  ADD UNIQUE KEY `platformname` (`platformname`);";
 
 $query .= "ALTER TABLE `roles`
   ADD PRIMARY KEY (`role_id`),
-  ADD UNIQUE KEY `Name` (`Name`); ";
+  ADD UNIQUE KEY `Name` (`Name`);";
 
 $query .= "ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `role_id` (`role_id`); ";
+  ADD KEY `role_id` (`role_id`);";
 
 
 $query .= "ALTER TABLE `dlc`
-  MODIFY `dlc_id` int(11) NOT NULL AUTO_INCREMENT; ";
+  MODIFY `dlc_id` int(11) NOT NULL AUTO_INCREMENT;";
 
 $query .= "ALTER TABLE `games`
-  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87; ";
+  MODIFY `game_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;";
 
 $query .= "ALTER TABLE `keys`
-  MODIFY `key_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;" ;
+  MODIFY `key_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;";
 
 $query .= "ALTER TABLE `platforms`
-  MODIFY `platform_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;" ;
+  MODIFY `platform_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;";
 
 $query .= "ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;" ;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;";
 
 
 $query .= "ALTER TABLE `keys`
   ADD CONSTRAINT `keys_ibfk_1` FOREIGN KEY (`dlc_id`) REFERENCES `dlc` (`dlc_id`),
   ADD CONSTRAINT `keys_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `games` (`game_id`),
   ADD CONSTRAINT `keys_ibfk_3` FOREIGN KEY (`platform_id`) REFERENCES `platforms` (`platform_id`),
-  ADD CONSTRAINT `keys_ibfk_4` FOREIGN KEY (`created_user_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL ON UPDATE SET NULL; ";
+  ADD CONSTRAINT `keys_ibfk_4` FOREIGN KEY (`created_user_id`) REFERENCES `users` (`user_id`) ON DELETE SET NULL ON UPDATE SET NULL;";
 
 $query .= "ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`); ";
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`);";
 
 $query .= "INSERT INTO USERS (username,password,forename,surname,email,role_id,create_date,approved) VALUES 
-    ( '$username','$password','$forename','$surname','$email',2,curdate(),1); ";
+    ( '$username','$password','$forename','$surname','$email',2,curdate(),1) ";
 
 
-if (!$mysqli->multi_query($query)) {
-    echo "Multi query failed: (" . $mysqli->errno . ") " . $mysqli->error;
-} else {
-	echo 'Database Created. Please Delete Install Directory! <a href="../index.php">Go Home</a>';
+$initdb = $mysqli->query($initdbsql);
+
+if ($mysqli->multi_query($query)) {
+    do {
+        /* store first result set */
+        if ($result = $mysqli->store_result()) {
+            while ($row = $result->fetch_row()) {
+                printf("%s\n", $row[0]);
+            }
+            $result->free();
+        }
+        /* print divider */
+        if ($mysqli->more_results()) {
+            printf("-----------------\n");
+        }
+    } while ($mysqli->next_result());
 }
-do {
-    if ($res = $mysqli->store_result()) {
-        var_dump($res->fetch_all(MYSQLI_ASSOC));
-        $res->free();
-    }
-} while ($mysqli->more_results() && $mysqli->next_result());
+
+else {
+    echo "Email address '$email' is considered invalid.\n";
+}
+}
 
 ?>
