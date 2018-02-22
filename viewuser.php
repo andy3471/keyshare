27a15bf1cc4dc3bda21b53216b9512869424654a
andy3471 <andy@insideindie.net>
2018-02-22 12:55:47 +0000

    getprofilepic

--- viewuser.php
+++ viewuser.php
@@ -49,6 +49,8 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo $row["username"].'</h2> <p> Karma: '.$row["karma"].'</p>';
 
+        $profile->getprofilepic($mysqli,$id);
+        echo '<br>';
 
         //Get Available Keys
         $fetchkeys = "
