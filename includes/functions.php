bd5a14369f867493997f0635f7204344dced70b8
andy3471 <andy@insideindie.net>
2018-02-22 13:11:13 +0000

    ProfilePicAdminFix

--- includes/functions.php
+++ includes/functions.php
@@ -16,6 +16,27 @@ class profile {
         while($row = $result->fetch_assoc()) {
                                         
         if (is_null($row["profilepic"])) {
+            echo '<a href=".\viewuser.php?id='.$x.'"><img src="./images/defaultpic.jpg" width="100px" height="100px" class="img-thumbnail"></a>';
+        } else {
+            echo '<a href=".\viewuser.php?id='.$x.'"><img src="./'.$row["profilepic"].'" width="100px" height="100px" class="img-thumbnail"></a>';
+            }
+        }
+        }
+    }
+    
+    
+    function getprofilepicadmin($mysqli,$x) {
+        $sql = "
+        SELECT profilepic from USERS
+        WHERE user_id = $x";
+
+        $result = $mysqli->query($sql);
+
+        if ($result->num_rows > 0) {
+        
+        while($row = $result->fetch_assoc()) {
+                                        
+        if (is_null($row["profilepic"])) {
             echo '<a href=".\viewuser.php?id='.$x.'"><img src="../images/defaultpic.jpg" width="100px" height="100px" class="img-thumbnail"></a>';
         } else {
             echo '<a href=".\viewuser.php?id='.$x.'"><img src="../'.$row["profilepic"].'" width="100px" height="100px" class="img-thumbnail"></a>';
