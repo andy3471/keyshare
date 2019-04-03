32363e1e4a3d8cf06a6cc6c3933d5fa873fc3084
Andrew Hargrave <andyjhargrave@protonmail.com>
2019-04-03 21:54:55 +0100

    ChangePasswordView

--- resources/views/auth/changepassword.blade.php
+++ resources/views/auth/changepassword.blade.php
@@ -6,13 +6,13 @@
     <form method="POST" action="{{ route('postpassword') }}">
         @csrf
         <label for="password">Current Password</label>
-        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="currentpassword" required>
 
         <label for="password">New Password</label>
-        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="newpassword" required>
 
         <label for="password">Confirm Password</label>
-        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="comfirmpassword" required>
 
         <br>
         <a class="btn btn-primary" type="submit">Change Password</a>
