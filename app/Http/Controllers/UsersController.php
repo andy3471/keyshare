c6e945dc2a864b1a604ba11e43ca7a38d11373c2
andy3471 <andy@insideindie.net>
2019-04-18 18:10:28 +0100

    Controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,14 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Users;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        $topusers = Redis::zrangebyscore('karma', 0, 10);
+        $karma = Redis::zrangebyscore('karma', 0, 9);
 
-        return $topusers;
+        $users = array();
+
+        foreach ( $karma as $user) {
+            $user = DB::table('users')
+                    ->select('id', 'name')
+                    ->where('id', '=', $user)
+                    ->get();
+
+            array_push($users, $user[0]);
+        }
+        return $users;
     }
 
     public function show($id)
