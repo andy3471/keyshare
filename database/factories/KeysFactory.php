d0ca02026785cfa194ff291dc7a08a552ef10524
andy3471 <andy@insideindie.net>
2019-04-23 18:42:09 +0100

    MigrateSeedsToFactories

---
+++ database/factories/KeysFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Keys;
+use Faker\Generator as Faker;
+use App\User;
+use App\Games;
+use App\Platforms;
+
+$factory->define(Keys::class, function (Faker $faker) {
+    return [
+        'game_id'           =>  Games::all()->random()->id,
+        'platform_id'       =>  Platforms::all()->random()->id,
+        'keycode'           =>  $faker->uuid,
+        'owned_user_id'     =>  User::all()->random()->id,
+        'created_user_id'   =>  User::all()->random()->id,
+        'message'           =>  $faker->paragraph($nbSentences = 1),
+    ];
+});
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,22 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
-
-        foreach(range(1, 20) as $index) {
-
-            if (rand (0,1) == 1) {
-                $bio = null;
-            } else {
-                $bio = $faker->paragraph($nbSentences = 1);
-            }
-
-            User::create([
-                'name'      =>  $faker->username,
-                'email'     =>  $faker->email,
-                'password'  =>  Hash::make('password'),
-                'bio'       =>  $bio
-            ]);
-        }
+        factory(App\User::class, 20)->create();
     }
 }
