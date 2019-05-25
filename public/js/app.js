48bdd6557e07e5c7f6ea5e1c890764c1d0ec2bea
andy3471 <andyjhargrave@protonmail.com>
2019-05-25 20:09:42 +0100

    ProductionServerFixes

--- public/js/app.js
+++ public/js/app.js
@@ -49670,8 +49670,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/andy/git/keyshare/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/andy/git/keyshare/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/keys/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/keys/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
