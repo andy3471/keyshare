a5646a8ba8e68b7dd0e2eea212cdb10406537dd7
Andrew Hargrave <Andrew.Hargrave@oneadvanced.com>
2020-01-24 17:25:19 +0000

    demomode

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,4 +29,9 @@ class HomeController extends Controller
     {
         return view('auth.notApproved');
     }
+
+    public function demo()
+    {
+        return view('auth.demomode');
+    }
 }
