18055041203ecd6b252c8774dfa78c25ea8fc09f
andy3471 <andy@insideindie.net>
2020-08-11 19:08:07 +0100

    Fixed #87 #24

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,9 +39,22 @@
             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
             <br>
-            <button type="submit" class="btn btn-keyshare">
-                {{ __('Register') }}
-            </button>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-8 offset-md-4">
+                    <button type="submit" class="btn btn-keyshare">
+                    {{ __('Register') }}
+                </button>
+
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                    @endif
+
+                    <a class="btn btn-link" href="{{ route('login') }}">Login</a>
+                </div>
+            </div>
         </form>
     </div>
 @endsection
