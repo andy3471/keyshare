cd511d0d969b3df109988f1aaec74864e7f2e302
Andrew Hargrave <me@andyh.app>
2020-08-13 10:40:26 +0100

    Fixed #86 #25

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,12 @@ set -e
 role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-production}
 
+echo "Clearing cache..."
+(cd /app && php artisan cache:clear && php artisan view:clear)
+
+echo "Running migrations..."
+(cd /app && php artisan migrate)
+
 if [ "$env" != "local" ]; then
     echo "Caching configuration..."
     (cd /app && php artisan optimize && php artisan view:cache)
@@ -17,11 +23,17 @@ if [ "$role" = "queue" ]; then
 
 elif [ "$role" = "scheduler" ]; then
 
+    echo "Running the scheduler..."
     while [ true ]
     do
       php /app/artisan schedule:run --verbose --no-interaction &
       sleep 60
     done
+
+elif [ "$role" = "keygen" ]; then
+
+    echo "Generating an app key..."
+    (cd /app && php artisan key:generate --show)
 
 else
     exec apache2-foreground
