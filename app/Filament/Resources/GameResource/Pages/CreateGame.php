ba4eda77522bb5abd688a5b29b0dbafe7dc82cf6
Andrew Hargrave <me@andyh.app>
2024-02-28 17:25:09 +0000

    add games to admin

---
+++ app/Filament/Resources/GameResource/Pages/CreateGame.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\GameResource\Pages;
+
+use App\Filament\Resources\GameResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateGame extends CreateRecord
+{
+    protected static string $resource = GameResource::class;
+}
--- routes/web.php
+++ routes/web.php
@@ -52,15 +52,3 @@ Route::middleware(['auth', 'approved'])->group(function () {
     Route::get('/games/dlc/get/{id}', 'DlcController@index');
     Route::get('/games/dlc/{dlc}', 'DlcController@show')->name('dlc');
 });
-
-//Route::middleware(['admin'])->group(function () {
-//    Route::get('/admin/users', 'AdminController@usersIndex')->name('adminshowusers');
-//    Route::get('/admin/user/{id}', 'AdminController@usersEdit')->name('adminuseredit')->middleware('demomode');
-//    Route::post('/admin/user/update', 'AdminController@usersUpdate')->name('adminuserupdate')->middleware('demomode');
-//
-//    Route::get('/games/dlc/edit/{dlc}', 'DlcController@edit')->name('editdlc');
-//    Route::post('/games/dlc/update', 'DlcController@update')->name('updatedlc');
-//
-//    Route::get('/games/edit/{id}', 'GameController@edit')->name('editgame');
-//    Route::post('/games/update', 'GameController@update')->name('updategame');
-//});
