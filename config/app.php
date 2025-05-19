6b2a3e821d654b4fb91260aafdf2aadd67d2a396
Shift <shift@laravelshift.com>
2025-05-19 15:19:42 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -9,31 +9,6 @@ return [
 
     'dlc_enabled' => env('DLC_ENABLED', true),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-        // SocialiteProviders
-        \SocialiteProviders\Manager\ServiceProvider::class,
-
-        // NotificationProviders
-        NotificationChannels\Discord\DiscordServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Redis' => Illuminate\Support\Facades\Redis::class,
