ec4add2acfa0be27bb41aa42155edc918701401f
Shift <shift@laravelshift.com>
2025-05-19 15:19:42 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -9,6 +10,15 @@ use View;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
     public function boot()
     {
         Model::preventLazyLoading(app()->isLocal());
@@ -21,5 +31,27 @@ class AppServiceProvider extends ServiceProvider
         if (app()->isProduction()) {
             URL::forceScheme('https');
         }
+
+        $this->bootAuth();
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    public function bootAuth()
+    {
+
+        Gate::define('approved', function ($user) {
+            return $user->approved;
+        });
+
+        Gate::define('admin', function ($user) {
+            return $user->admin;
+        });
     }
 }
