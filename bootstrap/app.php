8dd3fd495ce7f7b2816581701fd4651b7b92dd64
Shift <shift@laravelshift.com>
2025-05-19 15:19:41 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,33 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo('/home');
+
+        $middleware->encryptCookies(except: [
+            'XDEBUG_SESSION',
+        ]);
+
+        $middleware->append(\App\Http\Middleware\CheckForMaintenanceMode::class);
+
+        $middleware->throttleApi('60,1');
+
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+            'approved' => \App\Http\Middleware\Approved::class,
+            'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'demomode' => \App\Http\Middleware\DemoMode::class,
+            'steamlogin' => \App\Http\Middleware\SteamLoginEnabled::class,
+        ]);
+
+        $middleware->priority([
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\Authenticate::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Auth\Middleware\Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
