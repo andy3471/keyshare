9fdb753fcb053958971b51dd5390171043f2f0f1
Shift <shift@laravelshift.com>
2026-02-07 19:42:22 +0000

    Default new `bootstrap/app.php`

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,63 +1,21 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders([
-        SocialiteProviders\Manager\ServiceProvider::class,
-        NotificationChannels\Discord\DiscordServiceProvider::class,
-    ])
+    ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        //        api: __DIR__.'/../routes/api.php',
+        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+        // channels: __DIR__.'/../routes/channels.php',
         health: '/up',
-        then: function (): void {
-            Route::prefix('api')
-                // TODO: fix this
-                ->middleware('web')
-                ->group(base_path('routes/api.php'));
-        }
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->redirectGuestsTo(fn (): string => route('login'));
-        $middleware->redirectUsersTo('/games');
-
-        $middleware->encryptCookies(except: [
-            'XDEBUG_SESSION',
-        ]);
-
-        $middleware->web(append: [
-            App\Http\Middleware\HandleInertiaRequests::class,
-        ]);
-
-        $middleware->append(App\Http\Middleware\CheckForMaintenanceMode::class);
-
-        $middleware->throttleApi('60,1');
-
-        $middleware->alias([
-            'admin'      => App\Http\Middleware\Admin::class,
-            'approved'   => App\Http\Middleware\Approved::class,
-            'bindings'   => Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'demomode'   => App\Http\Middleware\DemoMode::class,
-            'steamlogin' => App\Http\Middleware\SteamLoginEnabled::class,
-        ]);
-
-        $middleware->priority([
-            Illuminate\Session\Middleware\StartSession::class,
-            Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            Illuminate\Session\Middleware\AuthenticateSession::class,
-            Illuminate\Routing\Middleware\SubstituteBindings::class,
-            Illuminate\Auth\Middleware\Authorize::class,
-        ]);
+    ->withMiddleware(function (Middleware $middleware) {
+        //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        if (class_exists(Sentry\Laravel\Integration::class)) {
-            Sentry\Laravel\Integration::handles($exceptions);
-        }
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
     })->create();
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
 ];
