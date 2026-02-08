8f8cd67df8c74ac237d8dfa3734e5afceeb66d0c
Andrew Hargrave <me@andyh.app>
2026-02-08 18:30:22 +0000

    fix xbox sign in

--- config/services.php
+++ config/services.php
@@ -49,6 +49,7 @@ return [
         'client_id'     => env('MICROSOFT_CLIENT_ID'),
         'client_secret' => env('MICROSOFT_CLIENT_SECRET'),
         'redirect'      => env('APP_URL').'/auth/xbox/callback',
+        'tenant'        => 'consumers',
     ],
 
 ];
