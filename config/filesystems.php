a25b832bf74c73b0e19052ed0bab5e732badbef5
Andrew Hargrave <me@andyh.app>
2026-02-18 18:32:10 +0000

    r2

--- config/filesystems.php
+++ config/filesystems.php
@@ -3,13 +3,35 @@
 declare(strict_types=1);
 
 return [
-
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+    'default' => env('FILESYSTEM_DISK', 'public'),
 
     'disks' => [
         'local' => [
             'driver' => 'local',
             'root'   => storage_path('app'),
+        ],
+
+        'public' => [
+            'driver'                  => 's3',
+            'key'                     => env('R2_ACCESS_KEY_ID'),
+            'secret'                  => env('R2_SECRET_ACCESS_KEY'),
+            'region'                  => 'auto',
+            'bucket'                  => env('R2_PUBLIC_BUCKET'),
+            'url'                     => env('R2_PUBLIC_URL'),
+            'endpoint'                => env('R2_ENDPOINT'),
+            'use_path_style_endpoint' => false,
+            'throw'                   => false,
+        ],
+
+        'private' => [
+            'driver'                  => 's3',
+            'key'                     => env('R2_ACCESS_KEY_ID'),
+            'secret'                  => env('R2_SECRET_ACCESS_KEY'),
+            'region'                  => 'auto',
+            'bucket'                  => env('R2_PRIVATE_BUCKET'),
+            'endpoint'                => env('R2_ENDPOINT'),
+            'use_path_style_endpoint' => false,
+            'throw'                   => false,
         ],
     ],
 
